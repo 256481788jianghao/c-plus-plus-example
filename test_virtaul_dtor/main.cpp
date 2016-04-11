22942c114d11010eca87d2dcf96f94ac58ff9cde
jianghao <256481788@163.com>
2016-04-11 08:31:59 +0800

    finish test_virtual_dtor

--- test_virtaul_dtor/main.cpp
+++ test_virtaul_dtor/main.cpp
@@ -4,16 +4,26 @@ using namespace std;
 class A{
 public:
     virtual int out()=0;
-    //virtual ~A(){ cout<<" A xigou"<<endl; }
+    virtual ~A(){ cout<<"release A"<<endl; }
 };
 
 class B:public A{
 public:
-    virtual ~B(){ cout<< "A xigou in B"<<endl;}
-    virtual int out(){ cout<<"this is b"<<endl; return 0; }
+    virtual ~B(){ 
+        cout<< "release B"<<endl;
+        delete m_p;
+    }
+    virtual int out(){ 
+        cout<<"this is b"<<endl; 
+        m_p = new int[5];
+        return 0; 
+    }
+private:
+    int *m_p;
 };
 int main(){
     A *pa = new B();
+    pa->out();
     delete pa;
     return 1;
 }
